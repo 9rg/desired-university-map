a5ec6d91060ebce006edfa608e5d71cc0cbd488e
9rg <danceNsleep@gmail.com>
2019-09-02 18:57:36 +0900

    なんとか動く状態

--- map.php
+++ map.php
@@ -39,55 +39,6 @@
         var map;//マップ用の変数
         var tokyo = {lat: 35.681167, lng: 139.767052};//マップで表示したい位置
         var marker; //マーカーのための変数
-        <?php echo "初期設定の最中"; ?>
-        var markerData = [
-          <?php
-          echo "地図に入った！";
-          if($type){
-            $mysqli = new mysqli( 'localhost', 'kuragane', 'VVmmjcU6TYTKJLQJ' ,'kuragane');
-            if( $mysqli->connect_errno ) {
-              echo $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
-            }
-
-            $dbc = mysqli_connect( 'localhost', 'kuragane', 'VVmmjcU6TYTKJLQJ', 'kuragane')
-            or die( 'エラー：MySQLサーバとの接続に失敗しました。');
-
-            $con = 'WHERE';
-
-            if($type){
-              $con .= " AND university_type = $type";
-            }
-            if($region){
-              $con .= " AND university_region = $region";
-            }
-            if($prefecture){
-              $con .= " AND university_prefecture = $prefecture";
-            }
-            if($faculty){
-              $con .= " AND university_faculty = $faculty";
-            }
-            if($graduate){
-              $con .=" AND university_graduate = $graduate";
-            }
-
-            $query = "SELECT * FROM university_table $con";
-            $result = mysqli_query($dbc, $query)
-            or die( 'エラー：データベースとの問い合わせに失敗しました。');
-
-            $count = count($result);
-
-            while($row = mysqli_fetch_array($result)){
-              $name = $row['university_name'];
-              $latitude = $row['university_latitude'];
-              $longitude = $row['university_longitude'];
-              $finalCon[] = "{name: '$name', lat: '$latitude', lng: '$longitude'}";
-            }
-            echo implode(',', $finalCon);
-          }
-          echo "地図のやつ終わった！";
-          ?>
-        ];
-
         map = new google.maps.Map(target,{
           center: tokyo,
           zoom: 10,
@@ -96,12 +47,12 @@
           clickableIcons: true
         });
       }
+      google.maps.event.addDomlistener( window, 'load', initMAP );
       </script>
     </div>
 
     <section class="searchParent">
       <?php
-      echo 'わっしょい';
       if(!$type){
         echo '<p class="message">下のボタンから<br>検索条件を設定してください</p>';
       }
